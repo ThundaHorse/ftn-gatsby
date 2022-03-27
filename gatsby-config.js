96ccd07b5697a69ad55b4eef86a31c66aed24850
Abe Kim <abe@bount.com>
2022-03-27 11:33:24 -0400

    small fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `Follow the Nerd`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Atlanta's leading career consulting service. You’ll be in good hands with our professional writers. Our team of writers has expertise in more than 65 industries and includes certified career coaches, recruiters, and experienced hiring professionals.`,
     author: `FTN`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
@@ -29,17 +29,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
-    // {
-    //   resolve: `gatsby-source-google-calendar`,
-    //   options: {
-    //     client_id: process.env.GOOGLE_CLIENT_ID,
-    //     apiKey: process.env.GOOGLE_CLIENT_SECRET,
-    //     calendarId: process.env.CALENDAR_ID,
-    //     scopes: process.env.GOOGLE_SCOPE,
-    //     discovery: process.env.GOOGLE_DISCOVERY_DOCS,
-    //     assumedUser: process.env.DEFAULT_EMAIL,
-    //   },
-    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
